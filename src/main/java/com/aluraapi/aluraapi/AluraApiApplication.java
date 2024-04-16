28f5ebf3eb13e1feb447307ce8f36b3457eea4e1
ThiagoLima <thiagoliimas@outlook.com>
2024-04-16 20:57:54 -0300

    feat: initial config

---
+++ src/main/java/com/aluraapi/aluraapi/AluraApiApplication.java
@@ -0,0 +1,13 @@
+package com.aluraapi.aluraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluraapi/aluraapi/AluraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluraapi.aluraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
