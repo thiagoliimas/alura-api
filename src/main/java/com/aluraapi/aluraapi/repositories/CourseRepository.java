afa84826fcc3238f7764b5ecec3cdb7e88f82c51
ThiagoLima <thiagoliimas@outlook.com>
2024-04-17 22:53:44 -0300

    feat: create CourseRepository.java

---
+++ src/main/java/com/aluraapi/aluraapi/repositories/CourseRepository.java
@@ -0,0 +1,17 @@
+package com.aluraapi.aluraapi.repositories;
+
+import com.aluraapi.aluraapi.domain.courses.Course;
+import com.aluraapi.aluraapi.infra.StatusEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    Optional<Course> findCourseByCode(String code);
+
+    Page<Course> findCoursesByStatus(StatusEnum status, Pageable pageable);
+}
